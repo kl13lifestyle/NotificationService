58fac40944a98d5fcf118d15e695caabdcedf356
mithun <kl13lifestyle@gmail.com>
2018-03-20 02:13:32 +0530

    First

--- app/src/main/java/com/example/mithun/notificationservice/MainActivity.java
+++ app/src/main/java/com/example/mithun/notificationservice/MainActivity.java
@@ -1,7 +1,13 @@
 package com.example.mithun.notificationservice;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,4 +16,38 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    private static final int REQUEST_READ_PHONE_STATE=0;
+
+    public void getPhoneStatePermission(View view) {
+        if (ContextCompat.checkSelfPermission(this,Manifest.permission.READ_PHONE_STATE)!= PackageManager.PERMISSION_GRANTED) {
+
+                ActivityCompat.requestPermissions(this,new String[]{Manifest.permission.READ_PHONE_STATE}, REQUEST_READ_PHONE_STATE);
+
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           String permissions[], int[] grantResults) {
+        switch (requestCode) {
+            case REQUEST_READ_PHONE_STATE: {
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+
+                    Toast.makeText(this,"Great. You can get notifications",Toast.LENGTH_SHORT).show();
+
+                } else {
+
+                    Toast.makeText(this,"Sorry you cant get notifications",Toast.LENGTH_SHORT).show();
+                }
+                return;
+            }
+
+            // other 'case' lines to check for other
+            // permissions this app might request
+        }
+    }
+
 }
