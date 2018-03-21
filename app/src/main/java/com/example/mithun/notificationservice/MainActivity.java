99a60933d4f30f4431a4a3d3ef58ad42ac841609
mithun <kl13lifestyle@gmail.com>
2018-03-22 01:49:05 +0530

    First

--- app/src/main/java/com/example/mithun/notificationservice/MainActivity.java
+++ app/src/main/java/com/example/mithun/notificationservice/MainActivity.java
@@ -1,14 +1,28 @@
 package com.example.mithun.notificationservice;
 
 import android.Manifest;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.ApplicationInfo;
+import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager;
+import android.content.pm.ResolveInfo;
+import android.graphics.drawable.Drawable;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -20,7 +34,8 @@ public class MainActivity extends AppCompatActivity {
 
     private static final int REQUEST_READ_PHONE_STATE=100;
     private static final int REQUEST_READ_SMS_PERMISSION=101;
-    private static final int REQUEST_ACCESS_NOTIFICATION_POLICY =102;
+    public ArrayList<appObject> applications = new ArrayList<appObject>();
+
 
     public void getPhoneStatePermission(View view) {
         if (ContextCompat.checkSelfPermission(this,Manifest.permission.READ_PHONE_STATE)!= PackageManager.PERMISSION_GRANTED) {
@@ -39,12 +54,9 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void getNotificationPermission(View view) {
-        if (ContextCompat.checkSelfPermission(this,Manifest.permission.ACCESS_NOTIFICATION_POLICY)!= PackageManager.PERMISSION_GRANTED) {
-            Log.v("inside getSMSPermission","inside get");
-            ActivityCompat.requestPermissions(this,new String[]{Manifest.permission.ACCESS_NOTIFICATION_POLICY}, REQUEST_ACCESS_NOTIFICATION_POLICY);
-            Log.v("getSMSPermissonLate",String.valueOf(REQUEST_READ_SMS_PERMISSION));
+        Intent intent=new Intent("android.settings.ACTION_NOTIFICATION_LISTENER_SETTINGS");
+        startActivity(intent);
 
-        }
     }
 
     @Override
@@ -55,11 +67,8 @@ public class MainActivity extends AppCompatActivity {
                 // If request is cancelled, the result arrays are empty.
                 if (grantResults.length > 0
                         && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-
                     Toast.makeText(this,"Great. You can get call notifications",Toast.LENGTH_SHORT).show();
-
                 } else {
-
                     Toast.makeText(this,"Sorry you cant get call notifications",Toast.LENGTH_SHORT).show();
                 }
                 return;
@@ -69,31 +78,51 @@ public class MainActivity extends AppCompatActivity {
                 // If request is cancelled, the result arrays are empty.
                 if (grantResults.length > 0
                         && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-
                     Toast.makeText(this,"Great. You can get sms notifications",Toast.LENGTH_SHORT).show();
-
                 } else {
-
                     Toast.makeText(this,"Sorry you cant get sms notifications",Toast.LENGTH_SHORT).show();
                 }
                 return;
             }
-            case REQUEST_ACCESS_NOTIFICATION_POLICY: {
-                // If request is cancelled, the result arrays are empty.
-                if (grantResults.length > 0
-                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-
-                    Toast.makeText(this,"Great. You can get notifications",Toast.LENGTH_SHORT).show();
-
-                } else {
-
-                    Toast.makeText(this,"Sorry you cant get notifications",Toast.LENGTH_SHORT).show();
-                }
-                return;
-            }
-            // other 'case' lines to check for other
-            // permissions this app might request
         }
     }
 
+
+    public void getApplicationList(View view){
+        final PackageManager pm = getPackageManager();
+        Intent intent = new Intent(Intent.ACTION_MAIN, null);
+        intent.addCategory(Intent.CATEGORY_LAUNCHER);
+        List<ResolveInfo> apps = pm.queryIntentActivities(intent, PackageManager.GET_META_DATA);
+
+       LayoutInflater linf=(LayoutInflater) getApplicationContext().getSystemService(
+                Context.LAYOUT_INFLATER_SERVICE);
+        linf = LayoutInflater.from(this);
+        LinearLayout rr =  findViewById(R.id.appList);
+
+
+        for (ResolveInfo ri: apps) {
+            Log.d("Installed App", "APP DETAILS:" + ri.activityInfo.applicationInfo.loadLabel(pm).toString() + " -> " +
+                    ri.activityInfo.applicationInfo.packageName);
+
+            appObject app=new appObject();
+            app.appName=ri.activityInfo.applicationInfo.loadLabel(pm).toString();
+            app.packageName=ri.activityInfo.applicationInfo.packageName;
+            app.icon=ri.activityInfo.loadIcon(pm);
+            applications.add(app);
+
+            ImageView icon = findViewById(R.id.appIcon);
+            icon.setImageDrawable(ri.activityInfo.loadIcon(pm));
+            final View v = linf.inflate(R.layout.app_list_item, null);
+
+            rr.addView(v);
+        }
+
+
+    }
+
+    public class appObject{
+        public String appName;
+        public String packageName;
+        public Drawable icon;
+    }
 }
--- app/src/main/java/com/example/mithun/notificationservice/NotificationListener.java
+++ app/src/main/java/com/example/mithun/notificationservice/NotificationListener.java
@@ -1,5 +1,40 @@
 package com.example.mithun.notificationservice;
 
+import android.content.Context;
+import android.content.Intent;
+import android.os.IBinder;
+import android.service.notification.NotificationListenerService;
+import android.service.notification.StatusBarNotification;
+import android.util.Log;
+import android.widget.Toast;
 
-public class NotificationListener {
+public class NotificationListener extends NotificationListenerService{
+    //public static String TAG = "NotificationListenerTesting";
+    //private StatusBarNotification[] mStatusBarNotification;
+    Context context;
+    @Override
+    public void onCreate(){
+        super.onCreate();
+        context = getApplicationContext();
+        Log.v("NotificationTesting", "Inside on create");
+    }
+    /*@Override
+    public IBinder onBind(Intent intent) {
+        return super.onBind(intent);
+    }*/
+    @Override
+    public void onNotificationPosted(StatusBarNotification sbn) {
+        Log.v("NotificationTesting", "Inside onNotificationPosted");
+        //TAG = "onNotificationPosted";
+        Log.v("onNotificationPosted", "Package Name : " + sbn.getPackageName() +
+                "Title : " + sbn.getNotification().extras.getString("android.title"));
+        Toast.makeText(context,sbn.getNotification().extras.getString("android.title"),Toast.LENGTH_SHORT).show();
+    }
+    @Override
+    public void onNotificationRemoved(StatusBarNotification sbn) {
+        //TAG = "onNotificationRemoved";
+        Log.v("onNotificationRemoved", "id = " + sbn.getId() + "Package Name" + sbn.getPackageName() +
+                "Post time = " + sbn.getPostTime() + "Tag = " + sbn.getTag());
+
+    }
 }
